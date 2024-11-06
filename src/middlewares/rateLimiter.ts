f09967595a66a5f634d5d1824a1c730a949aeeb6
AbhiramCheekala <134148054+AbhiramCheekala@users.noreply.github.com>
2024-11-06 21:43:18 +0530

    Update rateLimiter.ts

--- src/middlewares/rateLimiter.ts
+++ src/middlewares/rateLimiter.ts
@@ -1,14 +1,9 @@
 import rateLimit from 'express-rate-limit';
-
 export const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 20,
   skipSuccessfulRequests: true,
-  trustProxy: true, // Add this line to trust the X-Forwarded-For header
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-  keyGenerator: (req) => {
-    // Use X-Forwarded-For header if available, otherwise use the IP
-    return req.headers['x-forwarded-for'] || req.ip;
-  }
+  legacyHeaders: false, 
+  trustProxy: true, 
+  
 });
